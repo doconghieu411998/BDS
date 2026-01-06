4e2f198717b2b3ec4dddf30cc6c60cff24ffc68b
Đỗ Công Hiếu <doconghieu4198@gmail.com>
2026-01-06 21:21:26 +0700

    Updates

--- _next/static/9D3ILbB1vh8QoUIMMvFtA/_buildManifest.js
+++ _next/static/9D3ILbB1vh8QoUIMMvFtA/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/BDS//_next/:path+",
-        "destination": "/BDS/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
