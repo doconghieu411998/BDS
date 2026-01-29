8d76d43c877c92d5446824bff188848cdd257ec7
namnh <namnh@niw.com.vn>
2026-01-29 22:39:10 +0700

    fix nav menu

--- constants/menu.tsx
+++ constants/menu.tsx
@@ -1,6 +1,5 @@
 import {
     HomeOutlined,
-    UnorderedListOutlined,
     FileTextOutlined,
     FileSearchOutlined,
     GlobalOutlined,
@@ -23,7 +22,7 @@ export const MENU_ITEMS: MenuItem[] = [
         label: 'Trang chủ',
         path: ROUTES.DASHBOARD,
     },
-     {
+    {
         key: 'language',
         icon: <GlobalOutlined />,
         label: 'Quản lý ngôn ngữ',
@@ -33,27 +32,13 @@ export const MENU_ITEMS: MenuItem[] = [
         key: 'post',
         icon: <FileTextOutlined />,
         label: 'Quản lý Bài viết',
-        children: [
-            {
-                key: 'post-list',
-                icon: <UnorderedListOutlined />,
-                label: 'Danh sách bài viết',
-                path: ROUTES.POST.LIST,
-            },
-        ],
+        path: ROUTES.POST.LIST,
     },
     {
         key: 'consultation',
         icon: <FileSearchOutlined />,
         label: 'Khảo sát',
-        children: [
-            {
-                key: 'consultation-list',
-                icon: <UnorderedListOutlined />,
-                label: 'Danh sách khảo sát',
-                path: ROUTES.CONSULTATION.LIST,
-            },
-        ],
+        path: ROUTES.CONSULTATION.LIST,
     },
     {
         key: 'images',
