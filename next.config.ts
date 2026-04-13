495efdcd1c3cc3d89dfaa58d1ac4061321db6ba1
Đỗ Công Hiếu <doconghieu4198@gmail.com>
2026-04-13 23:44:30 +0700

    [UPDATE] config image to optiomize

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,8 @@ const config = (phase: string) => {
           port: "5000",
         },
       ],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
       formats: ["image/avif", "image/webp"],
     },
   };
