9513f39c066696bba4c606a485c8b4b187a53ec6
Đỗ Công Hiếu <doconghieu4198@gmail.com>
2026-05-02 15:22:32 +0700

    [UPDATE] SEO

--- app/robots.ts
+++ app/robots.ts
@@ -1,35 +1,11 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://theheraresort.com';
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/admin',
-      },
-      {
-        userAgent: 'facebookexternalhit',
-        allow: '/',
-      },
-      {
-        userAgent: 'meta-externalagent',
-        allow: '/',
-      },
-      {
-        userAgent: 'Twitterbot',
-        allow: '/',
-      },
-      {
-        userAgent: 'Discordbot',
-        allow: '/',
-      },
-      {
-        userAgent: 'Facebot',
-        allow: '/',
-      }
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://theheraresort.com/sitemap.xml',
   };
 }
